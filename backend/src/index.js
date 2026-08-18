db9fbbf83eeb6b4f597099d59ebba2976a98e8e1
Md Isaq Souraj <sourajmdisaq682@gmail.com>
2026-08-18 07:22:58 +0530

    Aded MiddleWare

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
+import cookieparser from "cookie-parser"
 
 dotenv.config();
 const app = express();
+
+app.use(express.json({limit:"100mb"}))
+
+app.use(urlencoded.json({limit:"100mb" , extended:true}))
+
+app.use(cookieparser.json())
+
 const PORT = process.env.PORT;
 
 app.get("/",(req,res)=>{
