7d6d3124578f2c6dc000bbc60dfa2f007a906826
Md Isaq Souraj <sourajmdisaq682@gmail.com>
2026-08-19 07:14:02 +0530

    DataBase Setup & userModel

--- backend/src/Models/userModel.js
+++ backend/src/Models/userModel.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-import validator from "validator";
+import validator, { isLowercase } from "validator";
 import bcrypt from "bcrypt";
-import crypto from "node:crypto";
+import crypto, { randomBytes } from "node:crypto";
+import { timeStamp } from "node:console";
 
 const userSchema = new mangoose.Schema({
 
@@ -10,5 +11,113 @@ const userSchema = new mangoose.Schema({
         required:[true, "Please Enter Your Name"],
         trim:true,
         maxLength:[100, "Your name should be under 100 character"]
+    },
+    email:{
+        type : String,
+        required:[true, "Please enter your email"],
+        unique:true,
+        lowercase:true,
+        trim:true,
+        validate:[validator.isEmail, "Please Enter valid email address"]
+    },
+    password:{
+        type : String,
+        required:[true, "Please enter the password"],
+        minlength:[8,"Your password should be atleast 8 character longer"],
+        maxlength:[20,"Your password should be atmost 20 character longer"],
+        select:false
+
+    },
+    passwordConfirm:{
+        type: String,
+        required:[true,"Please confirm your password"],
+        validate:{
+            validator:function(el){
+            el === this.password
+        },
+        message:"Passwords are not the same !"
     }
+},
+    phoneNumber:{
+        type: String,
+        required: true,
+        trim:true,
+        unique:true
+    },
+    role:{
+        type:String,
+        requred:true,
+        enum:["user","admin"],
+        default:"user"
+    },
+    avatar:{
+        url:{type:String},
+        public_id:{type:String}
+    },
+    passwordChangedAt:{
+        type:Date
+    },
+    passwordResetToken:{
+        type:String,
+        select:false,
+        index:true,
+    },
+    passwordResetExpired:{
+        select:false,
+        type:Date
+    }
+
+},
+{timestamps:true}
+)
+
+userSchema.set("toJSON",{
+        tranform: function(doc,ret){
+            delete ret.password;
+            delete ret.passwordConfirm;
+            delete ret.passwordResetExpired;
+            delete ret.passwordResetToken;
+            delete ret.__v;
+            return ret;
+        }
+    }
+)
+
+userSchema.pre("Save", async function (next){
+    if(!this.isModified("password") )return next();
+
+    this.password = bcrypt.hash(this.password,12)
+    this.passwordConfirm = undifined
+    next();
+
 })
+
+userSchema.methods.correctPassword = async function(candidatePassword,userPassword){
+ return await bcrypt.compare(candidatePassword,userPassword)
+}
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp){
+    if(this.passwordChangedAt){
+        const changedTimeStamp = preseInt(
+            this.passwordChangedAt.getTime()/1000,10
+        );
+    return JWTTimestamp < changedTimeStamp
+    
+    }
+    return false;
+
+}
+
+userSchema.methods.createPasswordResetToken = function(){
+    const resetToken = crypto.randomBytes(32).toString("hex")
+    this.passwordResetToken = crypto.createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+    this.passwordResetExpired = Date.now +10 *60*1000;
+    return resetToken;
+}
+
+
+const User = mongoose.Model("User",userSchema);
+
+export{User};
